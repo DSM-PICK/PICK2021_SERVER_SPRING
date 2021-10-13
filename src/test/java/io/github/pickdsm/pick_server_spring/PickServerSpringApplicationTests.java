a52b5e3e3ec7ce8e0359d9a4887835a6b628d530
leeseojune <sung07288346@gmail.com>
2021-10-13 23:53:20 +0900

    📑 :: 프로젝트 설정파일

---
+++ src/test/java/io/github/pickdsm/pick_server_spring/PickServerSpringApplicationTests.java
@@ -0,0 +1,14 @@
+package io.github.pickdsm.pick_server_spring;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickServerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
