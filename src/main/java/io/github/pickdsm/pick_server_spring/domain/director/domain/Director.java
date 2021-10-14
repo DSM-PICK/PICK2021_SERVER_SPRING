cde9d8043f4447a234d6abe0f10337e62b2ea52e
leeseojune <sung07288346@gmail.com>
2021-10-14 19:29:16 +0900

    📑 :: Director entity

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/director/domain/Director.java
@@ -0,0 +1,38 @@
+package io.github.pickdsm.pick_server_spring.domain.director.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import io.github.pickdsm.pick_server_spring.domain.teacher.domain.Teacher;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "tbl_director")
+public class Director {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "teacher_id")
+	private Teacher teacher;
+
+	private int floor;
+
+	@Builder
+	public Director(Teacher teacher, int floor) {
+		this.teacher = teacher;
+		this.floor = floor;
+	}
+
+}
