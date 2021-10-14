5cccfaa78584b6a3c8e7e6ab789620345713fd73
leeseojune <sung07288346@gmail.com>
2021-10-14 20:22:39 +0900

    📑 :: AffiliatedAfterSchool entity

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/affiliated_after_school/domain/AffiliatedAfterSchool.java
@@ -0,0 +1,41 @@
+package io.github.pickdsm.pick_server_spring.domain.affiliated_after_school.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import io.github.pickdsm.pick_server_spring.domain.after_school.domain.AfterSchool;
+import io.github.pickdsm.pick_server_spring.domain.student.domain.Student;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity(name = "tbl_affiliated_after_school")
+public class AffiliatedAfterSchool {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "student_id")
+	private Student student;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "after_school_id")
+	private AfterSchool afterSchool;
+
+	@Builder
+	public AffiliatedAfterSchool(Student student, AfterSchool afterSchool)  {
+		this.student = student;
+		this.afterSchool = afterSchool;
+	}
+
+}
