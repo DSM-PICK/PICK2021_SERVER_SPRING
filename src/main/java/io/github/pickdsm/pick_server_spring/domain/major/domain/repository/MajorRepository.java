c9f8ee2622ae51c37e4929a5078c8c7df6217de9
leeseojune <sung07288346@gmail.com>
2021-10-14 19:09:21 +0900

    📑 :: Major dao

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/major/domain/repository/MajorRepository.java
@@ -0,0 +1,8 @@
+package io.github.pickdsm.pick_server_spring.domain.major.domain.repository;
+
+import io.github.pickdsm.pick_server_spring.domain.major.domain.Major;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MajorRepository extends CrudRepository<Major, Long> {
+}
