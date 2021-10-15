7e908d6cb9671ae0ad0c0b8f8fb09f386cd655b3
leeseojune <sung07288346@gmail.com>
2021-10-15 23:28:29 +0900

    ⚡ :: Exception enum

--- src/main/java/io/github/pickdsm/pick_server_spring/global/error/exception/ErrorCode.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/global/error/exception/ErrorCode.java
@@ -1,15 +1,20 @@
 package io.github.pickdsm.pick_server_spring.global.error.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@JsonFormat(shape = Shape.OBJECT)
 public enum ErrorCode {
 
 	ALREADY_EXIST_TEACHER(400, "Already exist teacher."),
 	INVALID_CODE(401, "Invalid code."),
-	CREDENTIALS_NOT_FOUND(401, "Credentials not found.");
+	CREDENTIALS_NOT_FOUND(401, "Credentials not found."),
+	EXPIRED_ACCESS_TOKEN(401, "Expired access token."),
+	INVALID_TOKEN(401, "Invalid token.");
 
 	private final int status;
 	private final String message;
