5225d065367f057bfbbb0a601c60887ae33bc0ea
leeseojune <sung07288346@gmail.com>
2021-10-16 22:49:15 +0900

    ⚡ :: /teacher/password

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/teacher/presentation/TeacherController.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/teacher/presentation/TeacherController.java
@@ -3,19 +3,24 @@ package io.github.pickdsm.pick_server_spring.domain.teacher.presentation;
 import javax.validation.Valid;
 
 import io.github.pickdsm.pick_server_spring.domain.teacher.presentation.dto.request.LoginRequest;
+import io.github.pickdsm.pick_server_spring.domain.teacher.presentation.dto.request.PasswordRequest;
 import io.github.pickdsm.pick_server_spring.domain.teacher.presentation.dto.request.RegisterRequest;
 import io.github.pickdsm.pick_server_spring.domain.teacher.presentation.dto.response.InformationResponse;
 import io.github.pickdsm.pick_server_spring.domain.teacher.presentation.dto.response.TokenResponse;
+import io.github.pickdsm.pick_server_spring.domain.teacher.service.ChangePasswordService;
 import io.github.pickdsm.pick_server_spring.domain.teacher.service.LoginTeacherService;
 import io.github.pickdsm.pick_server_spring.domain.teacher.service.QueryInformationService;
 import io.github.pickdsm.pick_server_spring.domain.teacher.service.RegisterTeacherService;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,6 +31,7 @@ public class TeacherController {
 	private final RegisterTeacherService registerTeacherService;
 	private final LoginTeacherService loginTeacherService;
 	private final QueryInformationService queryInformationService;
+	private final ChangePasswordService changePasswordService;
 
 	@PostMapping("/register")
 	public TokenResponse registerTeacher(@RequestBody @Valid RegisterRequest request) {
@@ -40,6 +46,12 @@ public class TeacherController {
 	@GetMapping("/{teacher_id}/information")
 	public InformationResponse queryInformation(@PathVariable("teacher_id") String teacherId) {
 		return queryInformationService.execute(teacherId);
+	}
+
+	@PatchMapping("/password")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void changePassword(@RequestBody @Valid PasswordRequest request) {
+		changePasswordService.execute(request);
 	}
 
 }
