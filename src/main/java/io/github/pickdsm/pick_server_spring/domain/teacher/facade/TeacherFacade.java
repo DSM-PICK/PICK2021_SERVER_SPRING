765d2054f13b4521a72d3034090ab0d73046af7e
leeseojune <sung07288346@gmail.com>
2021-10-16 22:39:04 +0900

    ⚡ :: getCurrentTeacherId

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/teacher/facade/TeacherFacade.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/teacher/facade/TeacherFacade.java
@@ -3,8 +3,11 @@ package io.github.pickdsm.pick_server_spring.domain.teacher.facade;
 import io.github.pickdsm.pick_server_spring.domain.teacher.domain.Teacher;
 import io.github.pickdsm.pick_server_spring.domain.teacher.domain.repository.TeacherRepository;
 import io.github.pickdsm.pick_server_spring.domain.teacher.exception.CredentialsNotFoundException;
+import io.github.pickdsm.pick_server_spring.global.security.AuthDetails;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +19,15 @@ public class TeacherFacade {
 	public Teacher getTeacherById(String id) {
 		return teacherRepository.findById(id)
 				.orElseThrow(CredentialsNotFoundException::new);
+	}
+
+	public String getCurrentTeacherId() {
+		Object details = SecurityContextHolder
+				.getContext().getAuthentication().getPrincipal();
+		if(details instanceof AuthDetails) {
+			return ((AuthDetails)details).getTeacher().getId();
+		}
+		throw new CredentialsNotFoundException();
 	}
 
 }
