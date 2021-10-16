f36bab5ede918041deacd071e1315c447f0a9b3f
leeseojune <sung07288346@gmail.com>
2021-10-16 13:35:07 +0900

    ⚡ :: /teacher/{teacher_id}/information

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/teacher/presentation/TeacherController.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/teacher/presentation/TeacherController.java
@@ -4,11 +4,15 @@ import javax.validation.Valid;
 
 import io.github.pickdsm.pick_server_spring.domain.teacher.presentation.dto.request.LoginRequest;
 import io.github.pickdsm.pick_server_spring.domain.teacher.presentation.dto.request.RegisterRequest;
+import io.github.pickdsm.pick_server_spring.domain.teacher.presentation.dto.response.InformationResponse;
 import io.github.pickdsm.pick_server_spring.domain.teacher.presentation.dto.response.TokenResponse;
 import io.github.pickdsm.pick_server_spring.domain.teacher.service.LoginTeacherService;
+import io.github.pickdsm.pick_server_spring.domain.teacher.service.QueryInformationService;
 import io.github.pickdsm.pick_server_spring.domain.teacher.service.RegisterTeacherService;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +25,7 @@ public class TeacherController {
 
 	private final RegisterTeacherService registerTeacherService;
 	private final LoginTeacherService loginTeacherService;
+	private final QueryInformationService queryInformationService;
 
 	@PostMapping("/register")
 	public TokenResponse registerTeacher(@RequestBody @Valid RegisterRequest request) {
@@ -30,6 +35,11 @@ public class TeacherController {
 	@PostMapping("/login")
 	public TokenResponse loginTeacher(@RequestBody @Valid LoginRequest request) {
 		return loginTeacherService.execute(request);
+	}
+
+	@GetMapping("/{teacher_id}/information")
+	public InformationResponse queryInformation(@PathVariable("teacher_id") String teacherId) {
+		return queryInformationService.execute(teacherId);
 	}
 
 }
