cb2970591845c5acf2a19643a2a3f638b6b6f216
leeseojune <sung07288346@gmail.com>
2021-10-17 16:02:40 +0900

    📑 :: ChngeRequest dto

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/schedule/presentation/dto/request/ChangeRequest.java
@@ -0,0 +1,21 @@
+package io.github.pickdsm.pick_server_spring.domain.schedule.presentation.dto.request;
+
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ChangeRequest {
+
+	@NotNull(message = "date는 Null이면 안됩니다.")
+	private LocalDate date;
+
+	@NotEmpty(message = "name는 비어있으면 안됩니다.")
+	private String name;
+
+}
