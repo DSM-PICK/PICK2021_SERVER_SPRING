6d7f3e9d7f11bcc88a2e38d05f1a521dae269889
leeseojune <sung07288346@gmail.com>
2021-10-17 17:16:13 +0900

    📑 :: QueryScheduleService

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/schedule/service/QueryScheduleService.java
@@ -0,0 +1,26 @@
+package io.github.pickdsm.pick_server_spring.domain.schedule.service;
+
+import java.time.LocalDate;
+
+import io.github.pickdsm.pick_server_spring.domain.schedule.domain.repository.ScheduleRepository;
+import io.github.pickdsm.pick_server_spring.domain.schedule.exception.ScheduleNotFoundException;
+import io.github.pickdsm.pick_server_spring.domain.schedule.presentation.dto.response.ScheduleNameResponse;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class QueryScheduleService {
+
+	private final ScheduleRepository scheduleRepository;
+
+	public ScheduleNameResponse execute(LocalDate date) {
+		return new ScheduleNameResponse(
+				scheduleRepository.findByDate(date)
+						.orElseThrow(ScheduleNotFoundException::new)
+						.getName().name()
+		);
+	}
+
+}
