1a46b5042e95d3cf3e901c5b04c33a9d4998dbb7
leeseojune <sung07288346@gmail.com>
2021-10-17 16:18:41 +0900

    ⚡ :: POST /schedule

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/schedule/presentation/ScheduleController.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/schedule/presentation/ScheduleController.java
@@ -3,11 +3,14 @@ package io.github.pickdsm.pick_server_spring.domain.schedule.presentation;
 import javax.validation.Valid;
 
 import io.github.pickdsm.pick_server_spring.domain.schedule.presentation.dto.request.ChangeRequest;
+import io.github.pickdsm.pick_server_spring.domain.schedule.presentation.dto.request.CreateRequest;
 import io.github.pickdsm.pick_server_spring.domain.schedule.service.ChangeScheduleService;
+import io.github.pickdsm.pick_server_spring.domain.schedule.service.CreateScheduleService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,7 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ScheduleController {
 
+	private final CreateScheduleService createScheduleService;
 	private final ChangeScheduleService changeScheduleService;
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createSchedule(@RequestBody @Valid CreateRequest request) {
+		createScheduleService.execute(request);
+	}
 
 	@PatchMapping
 	@ResponseStatus(HttpStatus.NO_CONTENT)
