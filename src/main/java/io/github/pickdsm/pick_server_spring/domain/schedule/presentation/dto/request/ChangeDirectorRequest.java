2fef25555c2892fbc4bbd5cf6ff52af8c36512d3
leeseojune <sung07288346@gmail.com>
2021-10-17 19:49:54 +0900

    ⚡ :: Validator 추가

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/schedule/presentation/dto/request/ChangeDirectorRequest.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/schedule/presentation/dto/request/ChangeDirectorRequest.java
@@ -2,6 +2,11 @@ package io.github.pickdsm.pick_server_spring.domain.schedule.presentation.dto.re
 
 import java.time.LocalDate;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,8 +14,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ChangeDirectorRequest {
 
-	private int floor;
+	@Min(value = 3, message = "floor는 3이상의 값이어야 합니다.")
+	@Max(value = 5, message = "floor는 5이하의 값이어야 합니다.")
+	private Integer floor;
+
+	@NotNull(message = "date는 Null이면 안됩니다.")
 	private LocalDate date;
+
+	@NotEmpty(message = "teacher_id는 비어있으면 안됩니다.")
 	private String teacherId;
 
 }
