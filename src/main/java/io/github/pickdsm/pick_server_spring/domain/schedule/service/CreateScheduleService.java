b887b4ddb1ccb3cbdcf016600f4a54ee8af31ddd
leeseojune <sung07288346@gmail.com>
2021-10-17 16:18:20 +0900

    📑 :: CreateScheduleService

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/schedule/service/CreateScheduleService.java
@@ -0,0 +1,33 @@
+package io.github.pickdsm.pick_server_spring.domain.schedule.service;
+
+import io.github.pickdsm.pick_server_spring.domain.schedule.domain.Schedule;
+import io.github.pickdsm.pick_server_spring.domain.schedule.domain.repository.ScheduleRepository;
+import io.github.pickdsm.pick_server_spring.domain.schedule.domain.types.ScheduleName;
+import io.github.pickdsm.pick_server_spring.domain.schedule.exception.AlreadyExistScheduleException;
+import io.github.pickdsm.pick_server_spring.domain.schedule.presentation.dto.request.CreateRequest;
+import io.github.pickdsm.pick_server_spring.domain.schedule.utils.EnumUtils;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateScheduleService {
+
+	private final ScheduleRepository scheduleRepository;
+
+	public void execute(CreateRequest request) {
+		scheduleRepository
+				.findByDate(request.getDate())
+				.ifPresent(schedule -> {throw new AlreadyExistScheduleException();});
+		ScheduleName name = EnumUtils
+				.convertToScheduleName(request.getName());
+		scheduleRepository.save(
+				Schedule.builder()
+				.date(request.getDate())
+				.name(name)
+				.build()
+		);
+	}
+
+}
