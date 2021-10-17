2279baae579f882728b27a2868f66d274d98fc30
leeseojune <sung07288346@gmail.com>
2021-10-18 00:56:19 +0900

    📑 :: StudentFacade

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/student/facade/StudentFacade.java
@@ -0,0 +1,26 @@
+package io.github.pickdsm.pick_server_spring.domain.student.facade;
+
+import io.github.pickdsm.pick_server_spring.domain.student.domain.Student;
+import io.github.pickdsm.pick_server_spring.domain.student.domain.repository.StudentRepository;
+import io.github.pickdsm.pick_server_spring.domain.student.exception.StudentNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StudentFacade {
+
+	private final StudentRepository studentRepository;
+
+	public Student getStudentById(Long id) {
+		return studentRepository.findById(id)
+				.orElseThrow(StudentNotFoundException::new);
+	}
+
+	public Student getStudentByNameAndGcn(String name, String gcn) {
+		return studentRepository.findByNameAndGcn(name, gcn)
+				.orElseThrow(StudentNotFoundException::new);
+	}
+
+}
