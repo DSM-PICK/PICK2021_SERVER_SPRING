bacaa571c4e95af190580b4f8a7d1eaf917eb476
leeseojune <sung07288346@gmail.com>
2021-10-17 20:11:19 +0900

    ⚡ :: findAll 오버라이드

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/major/domain/repository/MajorRepository.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/major/domain/repository/MajorRepository.java
@@ -1,8 +1,12 @@
 package io.github.pickdsm.pick_server_spring.domain.major.domain.repository;
 
+import java.util.List;
+
 import io.github.pickdsm.pick_server_spring.domain.major.domain.Major;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface MajorRepository extends CrudRepository<Major, Long> {
+	@Override
+	List<Major> findAll();
 }
