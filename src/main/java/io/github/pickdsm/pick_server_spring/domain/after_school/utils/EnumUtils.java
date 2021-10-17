54e6327cf620585d354b9d624b8dcbeb4860c90a
leeseojune <sung07288346@gmail.com>
2021-10-18 00:52:13 +0900

    📑 :: 요일 Enum util

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/after_school/utils/EnumUtils.java
@@ -0,0 +1,16 @@
+package io.github.pickdsm.pick_server_spring.domain.after_school.utils;
+
+import io.github.pickdsm.pick_server_spring.domain.after_school.domain.types.Day;
+import io.github.pickdsm.pick_server_spring.global.exception.InvalidEnumValueException;
+
+public class EnumUtils {
+
+	public static Day convertToDay(String day) {
+		try {
+			return Day.valueOf(day);
+		} catch (IllegalArgumentException e) {
+			throw new InvalidEnumValueException();
+		}
+	}
+
+}
