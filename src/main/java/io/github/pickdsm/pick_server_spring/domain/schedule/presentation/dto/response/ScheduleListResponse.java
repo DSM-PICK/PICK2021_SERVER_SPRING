f80b88059e9c198125d5fdaf6ff4c34a2bb3a737
leeseojune <sung07288346@gmail.com>
2021-10-17 17:04:31 +0900

    📑 :: ScheduleListResponse

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/schedule/presentation/dto/response/ScheduleListResponse.java
@@ -0,0 +1,25 @@
+package io.github.pickdsm.pick_server_spring.domain.schedule.presentation.dto.response;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ScheduleListResponse {
+
+	private final LocalDate date;
+	private final String name;
+	private final List<Director> director;
+
+	@Getter
+	@AllArgsConstructor
+	public static class Director {
+		private final String name;
+		private final String teacherId;
+		private final int floor;
+	}
+
+}
