b4717971249795e7df2836255ba85949e74161e7
leeseojune <sung07288346@gmail.com>
2021-10-17 21:01:10 +0900

    ⚡ :: findByNameAndGcn

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/student/domain/repository/StudentRepository.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/student/domain/repository/StudentRepository.java
@@ -1,8 +1,11 @@
 package io.github.pickdsm.pick_server_spring.domain.student.domain.repository;
 
+import java.util.Optional;
+
 import io.github.pickdsm.pick_server_spring.domain.student.domain.Student;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
+	Optional<Student> findByNameAndGcn(String name, String gcn);
 }
