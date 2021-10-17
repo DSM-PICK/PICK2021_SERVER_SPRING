98d2ba5e12e737273b5d7ca217cfdfc86eebb8e5
leeseojune <sung07288346@gmail.com>
2021-10-17 16:01:22 +0900

    📑 :: findByDate

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/schedule/domain/repository/ScheduleRepository.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/schedule/domain/repository/ScheduleRepository.java
@@ -1,8 +1,12 @@
 package io.github.pickdsm.pick_server_spring.domain.schedule.domain.repository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 import io.github.pickdsm.pick_server_spring.domain.schedule.domain.Schedule;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface ScheduleRepository extends CrudRepository<Schedule, Long> {
+	Optional<Schedule> findByDate(LocalDate date);
 }
