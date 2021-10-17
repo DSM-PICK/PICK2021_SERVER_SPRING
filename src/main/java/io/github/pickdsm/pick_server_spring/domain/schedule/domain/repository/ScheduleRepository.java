5353a0339380d99bf581aa78eb2995b4af33841a
leeseojune <sung07288346@gmail.com>
2021-10-17 17:02:31 +0900

    ⚡ :: findByMonth

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/schedule/domain/repository/ScheduleRepository.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/schedule/domain/repository/ScheduleRepository.java
@@ -1,12 +1,17 @@
 package io.github.pickdsm.pick_server_spring.domain.schedule.domain.repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import io.github.pickdsm.pick_server_spring.domain.schedule.domain.Schedule;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ScheduleRepository extends CrudRepository<Schedule, Long> {
 	Optional<Schedule> findByDate(LocalDate date);
+
+	@Query("SELECT s FROM tbl_schedule s WHERE s.date LIKE CONCAT('%-', :month, '-%')")
+	List<Schedule> findByMonth(int month);
 }
