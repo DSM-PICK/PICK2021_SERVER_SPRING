178ba8670fe2104e3378fc6ca9cf75ca2c8e78b3
leeseojune <sung07288346@gmail.com>
2021-10-18 00:52:22 +0900

    📑 :: LocationFacade

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/location/facade/LocationFacade.java
@@ -0,0 +1,26 @@
+package io.github.pickdsm.pick_server_spring.domain.location.facade;
+
+import io.github.pickdsm.pick_server_spring.domain.location.domain.Location;
+import io.github.pickdsm.pick_server_spring.domain.location.domain.repository.LocationRepository;
+import io.github.pickdsm.pick_server_spring.domain.location.exception.LocationNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LocationFacade {
+
+	private final LocationRepository locationRepository;
+
+	public Location getLocationByName(String name) {
+		return locationRepository.findByName(name)
+				.orElseThrow(LocationNotFoundException::new);
+	}
+
+	public Location getLocationById(Long id) {
+		return locationRepository.findById(id)
+				.orElseThrow(LocationNotFoundException::new);
+	}
+
+}
