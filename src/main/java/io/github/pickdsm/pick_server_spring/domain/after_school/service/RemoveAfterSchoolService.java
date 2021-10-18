f53c8beefa565bb701a92353c961d92118074f7a
leeseojune <sung07288346@gmail.com>
2021-10-18 22:28:05 +0900

    📑 :: RemoveAfterSchoolService

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/after_school/service/RemoveAfterSchoolService.java
@@ -0,0 +1,28 @@
+package io.github.pickdsm.pick_server_spring.domain.after_school.service;
+
+import javax.transaction.Transactional;
+
+import io.github.pickdsm.pick_server_spring.domain.affiliated_after_school.domain.repository.AffiliatedAfterSchoolRepository;
+import io.github.pickdsm.pick_server_spring.domain.after_school.domain.repository.AfterSchoolRepository;
+import io.github.pickdsm.pick_server_spring.domain.after_school.exception.AfterSchoolNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RemoveAfterSchoolService {
+
+	private final AfterSchoolRepository afterSchoolRepository;
+	private final AffiliatedAfterSchoolRepository affiliatedAfterSchoolRepository;
+
+	@Transactional
+	public void execute(Long afterSchoolId) {
+		if(!afterSchoolRepository.existsById(afterSchoolId))
+			throw new AfterSchoolNotFoundException();
+
+		affiliatedAfterSchoolRepository.deleteByAfterSchool(afterSchoolId);
+		afterSchoolRepository.deleteById(afterSchoolId);
+	}
+
+}
