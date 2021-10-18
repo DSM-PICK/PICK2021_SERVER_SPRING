d3ad616bf966dac3335d1deb035ee53aff9c2187
leeseojune <sung07288346@gmail.com>
2021-10-18 22:02:23 +0900

    📑 :: UpdateAfterSchoolService

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/after_school/service/UpdateAfterSchoolService.java
@@ -0,0 +1,49 @@
+package io.github.pickdsm.pick_server_spring.domain.after_school.service;
+
+import javax.transaction.Transactional;
+
+import io.github.pickdsm.pick_server_spring.domain.after_school.domain.AfterSchool;
+import io.github.pickdsm.pick_server_spring.domain.after_school.domain.repository.AfterSchoolRepository;
+import io.github.pickdsm.pick_server_spring.domain.after_school.exception.AfterSchoolNotFoundException;
+import io.github.pickdsm.pick_server_spring.domain.after_school.presentation.dto.request.UpdateAfterSchoolRequest;
+import io.github.pickdsm.pick_server_spring.domain.after_school.utils.EnumUtils;
+import io.github.pickdsm.pick_server_spring.domain.location.facade.LocationFacade;
+import io.github.pickdsm.pick_server_spring.domain.teacher.facade.TeacherFacade;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateAfterSchoolService {
+
+	private final AfterSchoolRepository afterSchoolRepository;
+	private final LocationFacade locationFacade;
+	private final TeacherFacade teacherFacade;
+
+	@Transactional
+	public void execute(Long afterSchoolId, UpdateAfterSchoolRequest request) {
+		AfterSchool afterSchool = afterSchoolRepository
+				.findById(afterSchoolId)
+				.orElseThrow(AfterSchoolNotFoundException::new);
+		if(request.getName() != null) {
+			afterSchool.changeName(request.getName());
+		}
+		if(request.getDay() != null) {
+			afterSchool.changeDay(
+					EnumUtils.convertToDay(request.getDay())
+			);
+		}
+		if(request.getLocationId() != null) {
+			afterSchool.changeLocation(
+					locationFacade.getLocationById(request.getLocationId())
+			);
+		}
+		if(request.getTeacherId() != null) {
+			afterSchool.changeTeacher(
+					teacherFacade.getTeacherById(request.getTeacherId())
+			);
+		}
+	}
+
+}
