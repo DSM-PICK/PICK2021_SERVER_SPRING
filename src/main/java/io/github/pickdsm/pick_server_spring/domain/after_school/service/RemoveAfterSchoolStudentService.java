309286dbb112447f31e42aa0228cb50c1f1205c6
leeseojune <sung07288346@gmail.com>
2021-10-19 15:25:29 +0900

    📑 :: RemoveAfterSchoolStudentService

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/after_school/service/RemoveAfterSchoolStudentService.java
@@ -0,0 +1,32 @@
+package io.github.pickdsm.pick_server_spring.domain.after_school.service;
+
+import io.github.pickdsm.pick_server_spring.domain.affiliated_after_school.domain.repository.AffiliatedAfterSchoolRepository;
+import io.github.pickdsm.pick_server_spring.domain.affiliated_after_school.facade.AffiliatedAfterSchoolFacade;
+import io.github.pickdsm.pick_server_spring.domain.after_school.domain.AfterSchool;
+import io.github.pickdsm.pick_server_spring.domain.after_school.domain.repository.AfterSchoolRepository;
+import io.github.pickdsm.pick_server_spring.domain.after_school.exception.AfterSchoolNotFoundException;
+import io.github.pickdsm.pick_server_spring.domain.after_school.presentation.dto.request.RemoveAfterSchoolStudentRequest;
+import io.github.pickdsm.pick_server_spring.domain.student.domain.Student;
+import io.github.pickdsm.pick_server_spring.domain.student.facade.StudentFacade;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RemoveAfterSchoolStudentService {
+
+	private final AffiliatedAfterSchoolFacade affiliatedAfterSchoolFacade;
+	private final AfterSchoolRepository afterSchoolRepository;
+	private final StudentFacade studentFacade;
+
+	public void execute(Long afterSchoolId, RemoveAfterSchoolStudentRequest request) {
+		AfterSchool afterSchool = afterSchoolRepository
+				.findById(afterSchoolId)
+				.orElseThrow(AfterSchoolNotFoundException::new);
+		Student student = studentFacade
+				.getStudentById(request.getStudentId());
+		affiliatedAfterSchoolFacade.removeAffiliated(afterSchool, student);
+	}
+
+}
