f235d0c16ca44ef45bc56f939a8ab821131ae249
leeseojune <sung07288346@gmail.com>
2021-10-19 15:24:43 +0900

    📑 :: RemoveAfterSchoolStudentRequest Dto

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/after_school/presentation/dto/request/RemoveAfterSchoolStudentRequest.java
@@ -0,0 +1,15 @@
+package io.github.pickdsm.pick_server_spring.domain.after_school.presentation.dto.request;
+
+import javax.validation.constraints.NotNull;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class RemoveAfterSchoolStudentRequest {
+
+	@NotNull(message = "student_id는 Null이면 안됩니다.")
+	private Long studentId;
+
+}
