03fe28fb3318c2728acc1ba1604069ede2b0dc59
leeseojune <sung07288346@gmail.com>
2021-12-21 16:06:29 +0900

    ♻ :: 비밀번호 변경

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/teacher/service/ChangePasswordService.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/teacher/service/ChangePasswordService.java
@@ -1,16 +1,14 @@
 package io.github.pickdsm.pick_server_spring.domain.teacher.service;
 
-import javax.transaction.Transactional;
-
 import io.github.pickdsm.pick_server_spring.domain.teacher.domain.Teacher;
 import io.github.pickdsm.pick_server_spring.domain.teacher.domain.repository.TeacherRepository;
 import io.github.pickdsm.pick_server_spring.domain.teacher.exception.CredentialsNotFoundException;
-import io.github.pickdsm.pick_server_spring.domain.teacher.facade.TeacherFacade;
-import io.github.pickdsm.pick_server_spring.domain.teacher.presentation.dto.request.PasswordRequest;
+import io.github.pickdsm.pick_server_spring.domain.teacher.presentation.dto.request.ChangePasswordRequest;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,12 +16,11 @@ public class ChangePasswordService {
 
 	private final TeacherRepository teacherRepository;
 	private final PasswordEncoder passwordEncoder;
-	private final TeacherFacade teacherFacade;
 
 	@Transactional
-	public void execute(PasswordRequest request) {
+	public void execute(ChangePasswordRequest request) {
 		Teacher teacher = teacherRepository
-				.findById(teacherFacade.getCurrentTeacherId())
+				.findById(request.getTeacherId())
 				.orElseThrow(() -> CredentialsNotFoundException.EXCEPTION);
 		teacher.changePassword(
 				passwordEncoder.encode(request.getPassword())
