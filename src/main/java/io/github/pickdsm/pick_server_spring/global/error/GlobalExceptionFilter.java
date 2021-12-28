1bab8500e435edba88d283808d806174dfe9ed06
leeseojune <sung07288346@gmail.com>
2021-12-28 09:45:57 +0900

    ⚡ :: Exception filtere add Cors header

--- src/main/java/io/github/pickdsm/pick_server_spring/global/error/GlobalExceptionFilter.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/global/error/GlobalExceptionFilter.java
@@ -1,17 +1,15 @@
 package io.github.pickdsm.pick_server_spring.global.error;
 
-import java.io.IOException;
+import io.github.pickdsm.pick_server_spring.global.error.exception.ErrorCode;
+import io.github.pickdsm.pick_server_spring.global.error.exception.PickException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import io.github.pickdsm.pick_server_spring.global.error.exception.ErrorCode;
-import io.github.pickdsm.pick_server_spring.global.error.exception.PickException;
-
-import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
 
 @Component
 public class GlobalExceptionFilter extends OncePerRequestFilter {
@@ -24,6 +22,14 @@ public class GlobalExceptionFilter extends OncePerRequestFilter {
 			ErrorCode errorCode = e.getErrorCode();
 			ErrorResponse errorResponse =
 					new ErrorResponse(errorCode.getStatus(), errorCode.getCode(), errorCode.getMessage());
+
+			response.setHeader("Access-Control-Allow-Origin", "*");
+			response.setHeader("Access-Control-Allow-Credentials", "true");
+			response.setHeader("Access-Control-Allow-Methods","*");
+			response.setHeader("Access-Control-Max-Age", "3600");
+			response.setHeader("Access-Control-Allow-Headers",
+					"Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
 			response.setStatus(errorCode.getStatus());
 			response.setContentType("application/json");
 			response.getWriter().write(errorResponse.toString());
