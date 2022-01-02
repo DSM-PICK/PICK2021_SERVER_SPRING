c7805699e07d5b6c40790007ee775ac939f971eb
leeseojune <sung07288346@gmail.com>
2022-01-02 16:46:25 +0900

    ⚡ :: findByAfterSchool

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/affiliated_after_school/domain/repository/AffiliatedAfterSchoolRepository.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/affiliated_after_school/domain/repository/AffiliatedAfterSchoolRepository.java
@@ -1,14 +1,14 @@
 package io.github.pickdsm.pick_server_spring.domain.affiliated_after_school.domain.repository;
 
-import java.util.List;
-import java.util.Optional;
-
 import io.github.pickdsm.pick_server_spring.domain.affiliated_after_school.domain.AffiliatedAfterSchool;
+import io.github.pickdsm.pick_server_spring.domain.after_school.domain.AfterSchool;
 import io.github.pickdsm.pick_server_spring.domain.student.domain.Student;
-
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface AffiliatedAfterSchoolRepository extends CrudRepository<AffiliatedAfterSchool, Long> {
 
@@ -20,5 +20,7 @@ public interface AffiliatedAfterSchoolRepository extends CrudRepository<Affiliat
 	Optional<AffiliatedAfterSchool> findByAfterSchoolAndStudent(Long afterSchoolId, Long studentId);
 
 	List<AffiliatedAfterSchool> findByStudent(Student student);
+
+	List<AffiliatedAfterSchool> findByAfterSchool(AfterSchool afterSchool);
 
 }
