927471d8a0f0d35d8209912db984553a4c20027b
leeseojune53 <sung07288346@gmail.com>
2022-06-23 19:33:02 +0900

    ⚡️ :: Major findByLocation

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/major/domain/repository/MajorRepository.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/major/domain/repository/MajorRepository.java
@@ -3,6 +3,7 @@ package io.github.pickdsm.pick_server_spring.domain.major.domain.repository;
 import java.util.List;
 import java.util.Optional;
 
+import io.github.pickdsm.pick_server_spring.domain.location.domain.Location;
 import io.github.pickdsm.pick_server_spring.domain.major.domain.Major;
 
 import org.springframework.data.repository.CrudRepository;
@@ -10,5 +11,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface MajorRepository extends CrudRepository<Major, Long> {
 	@Override
 	List<Major> findAll();
+	Optional<Major> findByLocation(Location location);
 	Optional<Major> findByName(String name);
 }
