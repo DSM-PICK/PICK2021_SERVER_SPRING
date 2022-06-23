3ce41d9511f36c778b974866afa24a088e2aaa20
leeseojune53 <sung07288346@gmail.com>
2022-06-23 15:13:06 +0900

    ⚡️ :: DELETE /attendance/{attendanceId}

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/attendance/presentation/AttendanceController.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/attendance/presentation/AttendanceController.java
@@ -2,11 +2,14 @@ package io.github.pickdsm.pick_server_spring.domain.attendance.presentation;
 
 import io.github.pickdsm.pick_server_spring.domain.attendance.presentation.dto.request.PostAttendanceRequest;
 import io.github.pickdsm.pick_server_spring.domain.attendance.presentation.dto.response.QueryTodayAttendanceResponse;
+import io.github.pickdsm.pick_server_spring.domain.attendance.service.DeleteAttendanceService;
 import io.github.pickdsm.pick_server_spring.domain.attendance.service.PostAttendanceService;
 import io.github.pickdsm.pick_server_spring.domain.attendance.service.QueryTodayAttendanceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +26,7 @@ public class AttendanceController {
 
     private final PostAttendanceService postAttendanceService;
     private final QueryTodayAttendanceService queryTodayAttendanceService;
+    private final DeleteAttendanceService deleteAttendanceService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
@@ -33,6 +37,12 @@ public class AttendanceController {
     @GetMapping("/today")
     public List<QueryTodayAttendanceResponse> queryTodayAttendance(@RequestParam("floor") int floor) {
         return queryTodayAttendanceService.queryTodayAttendance(floor);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{attendanceId}")
+    public void deleteAttendance(@PathVariable("attendanceId") Long attendanceId) {
+        deleteAttendanceService.deleteAttendance(attendanceId);
     }
 
 
