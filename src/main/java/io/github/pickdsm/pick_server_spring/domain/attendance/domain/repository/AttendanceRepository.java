34f73aff124bb3bdf079e418fc2011ac52558a27
leeseojune53 <sung07288346@gmail.com>
2022-06-23 19:30:57 +0900

    ⚡️ :: findAttendance

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/attendance/domain/repository/AttendanceRepository.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/attendance/domain/repository/AttendanceRepository.java
@@ -2,13 +2,23 @@ package io.github.pickdsm.pick_server_spring.domain.attendance.domain.repository
 
 import io.github.pickdsm.pick_server_spring.domain.attendance.domain.Attendance;
 
+import io.github.pickdsm.pick_server_spring.domain.attendance.domain.repository.vo.StudentInfoVO;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 public interface AttendanceRepository extends CrudRepository<Attendance, Long> {
     @Query("SELECT a FROM tbl_attendance a WHERE a.date = :date AND a.location.floor = :floor")
     List<Attendance> findByFloor(LocalDate date, int floor);
+
+    @Query("SELECT a FROM tbl_attendance a WHERE a.date = :date AND a.location.id = :locationId")
+    List<Attendance> findByLocationId(LocalDate date, Long locationId);
+
+    @Query("SELECT NEW io.github.pickdsm.pick_server_spring." +
+            "domain.attendance.domain.repository.vo.StudentInfoVO(a.student.gcn, a.student.id, a.student.name) " +
+            "FROM tbl_attendance a WHERE a.date = :date AND a.location.id = :locationId")
+    Set<StudentInfoVO> findStudentByLocationId(LocalDate date, Long locationId);
 }
