1e77ce45980b4004f255bb728bb5c990aa2b0c6c
leeseojune53 <sung07288346@gmail.com>
2022-06-26 21:55:22 +0900

    ⚡️ :: PATCH /{locationId}

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/location/presentation/LocationController.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/location/presentation/LocationController.java
@@ -1,13 +1,16 @@
 package io.github.pickdsm.pick_server_spring.domain.location.presentation;
 
 import io.github.pickdsm.pick_server_spring.domain.location.presentation.dto.request.CreateLocationRequest;
+import io.github.pickdsm.pick_server_spring.domain.location.presentation.dto.request.UpdateLocationRequest;
 import io.github.pickdsm.pick_server_spring.domain.location.presentation.dto.response.QueryLocationListResponse;
 import io.github.pickdsm.pick_server_spring.domain.location.service.CreateLocationService;
 import io.github.pickdsm.pick_server_spring.domain.location.service.QueryLocationListService;
 import io.github.pickdsm.pick_server_spring.domain.location.service.QuerySpecificFloorLocationService;
+import io.github.pickdsm.pick_server_spring.domain.location.service.UpdateLocationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +28,7 @@ public class LocationController {
     private final QuerySpecificFloorLocationService querySpecificFloorLocationService;
     private final QueryLocationListService queryLocationListService;
     private final CreateLocationService createLocationService;
+    private final UpdateLocationService updateLocationService;
 
     @GetMapping("/{floor}")
     public List<QueryLocationListResponse> querySpecificFloorLocationList(@PathVariable("floor") int floor) {
@@ -40,6 +44,12 @@ public class LocationController {
     @PostMapping
     public void createLocation(@RequestBody CreateLocationRequest request) {
         createLocationService.createLocation(request);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/{locationId}")
+    public void updateLocation(@PathVariable("locationId") Long locationId, @RequestBody UpdateLocationRequest request) {
+        updateLocationService.updateLocation(locationId, request);
     }
 
 }
