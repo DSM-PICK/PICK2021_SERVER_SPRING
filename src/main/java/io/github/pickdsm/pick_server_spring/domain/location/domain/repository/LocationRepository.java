5f5276dc19a6759e54e817653a0e96cb8c873346
leeseojune53 <sung07288346@gmail.com>
2022-06-27 17:04:03 +0900

    ⚡️ :: findAllClass

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/location/domain/repository/LocationRepository.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/location/domain/repository/LocationRepository.java
@@ -5,10 +5,13 @@ import java.util.Optional;
 
 import io.github.pickdsm.pick_server_spring.domain.location.domain.Location;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LocationRepository extends CrudRepository<Location, Long> {
 	Optional<Location> findByName(String name);
 	List<Location> findByFloor(int floor);
 	List<Location> findAll();
+	@Query("SELECT l FROM tbl_location l WHERE l.name LIKE '_-_'")
+	List<Location> findAllClass();
 }
