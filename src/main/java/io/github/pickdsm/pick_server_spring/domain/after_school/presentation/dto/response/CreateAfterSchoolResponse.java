98ec9879e00b44e1d030741a4b4089e8bf2c167c
leeseojune53 <sung07288346@gmail.com>
2022-07-04 19:51:47 +0900

    📑 :: CreateAfterSchoolResponse dto

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/after_school/presentation/dto/response/CreateAfterSchoolResponse.java
@@ -0,0 +1,12 @@
+package io.github.pickdsm.pick_server_spring.domain.after_school.presentation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateAfterSchoolResponse {
+
+    private final Long afterSchoolId;
+
+}
