343e1dd1988123417f1c0ae3ab256c36dc6db04d
leeseojune53 <sung07288346@gmail.com>
2022-07-05 19:54:28 +0900

    ⚡️ :: error handling

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/major/service/CreateMajorService.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/major/service/CreateMajorService.java
@@ -4,6 +4,7 @@ import io.github.pickdsm.pick_server_spring.domain.location.domain.Location;
 import io.github.pickdsm.pick_server_spring.domain.location.facade.LocationFacade;
 import io.github.pickdsm.pick_server_spring.domain.major.domain.Major;
 import io.github.pickdsm.pick_server_spring.domain.major.domain.repository.MajorRepository;
+import io.github.pickdsm.pick_server_spring.domain.major.exception.AlreadyExistMajorLocationException;
 import io.github.pickdsm.pick_server_spring.domain.major.exception.AlreadyExistMajorNameException;
 import io.github.pickdsm.pick_server_spring.domain.major.presentation.dto.request.CreateMajorRequest;
 import io.github.pickdsm.pick_server_spring.domain.major.presentation.dto.response.PostMajorResponse;
@@ -20,33 +21,42 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 public class CreateMajorService {
 
-	private final StudentFacade studentFacade;
-	private final LocationFacade locationFacade;
-	private final TeacherFacade teacherFacade;
-	private final MajorRepository majorRepository;
+    private final StudentFacade studentFacade;
+    private final LocationFacade locationFacade;
+    private final TeacherFacade teacherFacade;
+    private final MajorRepository majorRepository;
 
-	@Transactional
-	public PostMajorResponse execute(CreateMajorRequest request) {
-		majorRepository.findByName(request.getMajorName())
-				.ifPresent(major -> {throw AlreadyExistMajorNameException.EXCEPTION;});
-		Student head = studentFacade
-				.getStudentByNameAndGcn(request.getHeadName(), request.getHeadGcn());
-		Location location = locationFacade
-				.getLocationById(request.getLocationId());
-		Teacher teacher = teacherFacade
-				.getTeacherById(request.getTeacherId());
+    @Transactional
+    public PostMajorResponse execute(CreateMajorRequest request) {
+        majorRepository.findByName(request.getMajorName())
+                .ifPresent(major -> {
+                    throw AlreadyExistMajorNameException.EXCEPTION;
+                });
+        Student head = studentFacade
+                .getStudentByNameAndGcn(request.getHeadName(), request.getHeadGcn());
+        Location location = locationFacade
+                .getLocationById(request.getLocationId());
+        Teacher teacher = teacherFacade
+                .getTeacherById(request.getTeacherId());
+        Major major;
 
-		Major major = majorRepository.save(
-				Major.builder()
-				.name(request.getMajorName())
-				.head(head)
-				.location(location)
-				.teacher(teacher)
-				.build()
-		);
-		head.changeMajor(major);
+        try {
+            major = majorRepository.save(
+                    Major.builder()
+                            .name(request.getMajorName())
+                            .head(head)
+                            .location(location)
+                            .teacher(teacher)
+                            .build()
+            );
+        } catch (RuntimeException e) {
+            throw AlreadyExistMajorLocationException.EXCEPTION;
+        }
 
-		return new PostMajorResponse(major.getId());
-	}
+
+        head.changeMajor(major);
+
+        return new PostMajorResponse(major.getId());
+    }
 
 }
