fd620b962f67bb6f5e596788251b4989388987f4
정대현 <eogus_2513@dsm.hs.kr>
2022-10-12 19:41:09 +0900

    📑 :: CreateTeacherRequest

---
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/teacher/presentation/dto/request/CreateTeacherRequest.java
@@ -0,0 +1,10 @@
+package io.github.pickdsm.pick_server_spring.domain.teacher.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class CreateTeacherRequest {
+    private String name;
+}
