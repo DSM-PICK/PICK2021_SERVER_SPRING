de3178f86c0fb9e65f44412ff9d9ffefafe1bc3b
정대현 <eogus_2513@dsm.hs.kr>
2022-10-12 19:44:20 +0900

    :zap: findTop1ByOrderByIdDesc & existsById 기능 추가

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/teacher/domain/repository/TeacherRepository.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/teacher/domain/repository/TeacherRepository.java
@@ -2,11 +2,14 @@ package io.github.pickdsm.pick_server_spring.domain.teacher.domain.repository;
 
 import io.github.pickdsm.pick_server_spring.domain.location.domain.Location;
 import io.github.pickdsm.pick_server_spring.domain.teacher.domain.Teacher;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface TeacherRepository extends JpaRepository<Teacher, String> {
     Optional<Teacher> findByLocation(Location location);
+
+    Teacher findTop1ByOrderByIdDesc();
+
+    boolean existsById(String id);
 }
