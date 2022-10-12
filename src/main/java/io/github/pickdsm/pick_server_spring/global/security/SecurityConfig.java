9282335a924f4e1566b59afa0f79499e3d1c225d
정대현 <eogus_2513@dsm.hs.kr>
2022-10-12 19:37:09 +0900

    :zap: SecurityCofig 설정

--- src/main/java/io/github/pickdsm/pick_server_spring/global/security/SecurityConfig.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/global/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package io.github.pickdsm.pick_server_spring.global.security;
 import io.github.pickdsm.pick_server_spring.global.error.GlobalExceptionFilter;
 import io.github.pickdsm.pick_server_spring.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -15,36 +14,35 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsUtils;
 
-@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	private final JwtTokenProvider jwtTokenProvider;
-	private final GlobalExceptionFilter globalExceptionFilter;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final GlobalExceptionFilter globalExceptionFilter;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-				.csrf().disable()
-				.formLogin().disable()
-				.sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http
-				.authorizeRequests()
-				.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-				.antMatchers(HttpMethod.POST, "/teacher/register").permitAll()
-				.antMatchers(HttpMethod.POST, "/teacher/login").permitAll()
-				.antMatchers(HttpMethod.PUT, "/teacher/auth").permitAll()
-				.antMatchers(HttpMethod.GET, "/teacher/list").permitAll()
-				.antMatchers(HttpMethod.POST, "/major").hasRole("ADMIN")
-				.anyRequest().authenticated()
-				.and().apply(new FilterConfig(jwtTokenProvider, globalExceptionFilter));
-	}
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .antMatchers(HttpMethod.POST, "/teacher/register").permitAll()
+                .antMatchers(HttpMethod.POST, "/teacher/login").permitAll()
+                .antMatchers(HttpMethod.PUT, "/teacher/auth").permitAll()
+                .antMatchers(HttpMethod.GET, "/teacher/list").permitAll()
+                .antMatchers(HttpMethod.POST, "/major").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and().apply(new FilterConfig(jwtTokenProvider, globalExceptionFilter));
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
