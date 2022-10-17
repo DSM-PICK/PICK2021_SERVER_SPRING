34640f2f4be03048e4517249198c2dc40a755baa
정대현 <eogus_2513@dsm.hs.kr>
2022-10-17 16:53:56 +0900

    🐛 :: bug resolve

--- src/main/java/io/github/pickdsm/pick_server_spring/domain/attendance/service/QueryAttendanceService.java
+++ src/main/java/io/github/pickdsm/pick_server_spring/domain/attendance/service/QueryAttendanceService.java
@@ -63,7 +63,7 @@ public class QueryAttendanceService {
             studentInfoVOList = attendanceRepository.findMajorStudyStudentByLocationId(locationId);
             attendances = attendanceRepository.findByMajorLocationId(date, locationId);
         } else if(ScheduleName.AFTER_SCHOOL.equals(schedule.getName())) {
-            AfterSchool afterSchool = afterSchoolRepository.findByLocationAndDay(location, date.getDayOfWeek().toString().substring(0, 2))
+            AfterSchool afterSchool = afterSchoolRepository.findByLocationAndDay(location, date.getDayOfWeek().toString().substring(0, 3))
                     .orElseThrow(() -> AfterSchoolNotFoundException.EXCEPTION);
 
             className = afterSchool.getName();
